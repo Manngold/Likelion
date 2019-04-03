7b56b7f1a0f855ab0cdf20bd76170f7e1b0d0f0b
Manngold <jsw4820@gmail.com>
2019-04-03 18:01:25 +0900

    python syntax and html css final

--- python/test.py
+++ python/test.py
@@ -1,2 +1,23 @@
-for a in [1, 3, 5, 7, 8]:
-    print(a)
+a = "Life is too short, you need python"
+
+print(type(a))
+print(len(a))
+print(type(a[0: 4]))
+
+today = "20190331Rainy"
+
+year = today[:4]
+day = today[4:8]
+weather = today[8:]
+
+print(year)
+print(day)
+print(weather)
+
+num = [1, 2, 3, [4, 5, 6]]
+print(num[3])
+print(num)
+
+strlist = ["c", "a", "d", "b"]
+strlist.sort(reverse=True)
+print(strlist)
